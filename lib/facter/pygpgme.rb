21e131583fc0d72c4b078154781897fd0230eeaa
Anton Samets <a.samets@gmail.com>
2024-09-25 14:34:11 +0300

    speed up fact pygpgme by correct call.

--- lib/facter/pygpgme.rb
+++ lib/facter/pygpgme.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 Facter.add('pygpgme_installed') do
   setcode do
-    os = Facter.value(:operatingsystem)
-    case os.downcase
+    os = Facter.value(:operatingsystem).downcase
+
+    case os
     when /debian|ubuntu|windows/
       'true'
     else
-      output = Facter::Core::Execution.exec('rpm -qa | grep pygpgme')
-      if !/^pygpgme.*/.match(output).nil?
+      output = Facter::Core::Execution.exec('rpm -q pygpgme')
+      if output.start_with?('pygpgme')
         'true'
       else
         'false'
